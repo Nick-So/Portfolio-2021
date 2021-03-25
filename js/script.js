627467c19e693f32358352297b77d6119ee432ff
NickSo-Design <contact@nickso.com>
2021-03-24 18:27:57 -0700

    Updated buttons, click outside modal to close

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log('ready');
 
-  $("#hamburger").on('click', function() {
+  /*Navigation Slider*/
+  $("#hamburger").on('click', function () {
     $('#nav').slideToggle();
   });
 
-  $('.card').on('click', function() {
-      var id = $(this).attr('id');
-      console.log(id);
-      $('.overlayContainer').css('display', 'flex').hide().fadeIn(300);
-      $(`#${id}.overlay`).removeClass('inactive').addClass('active');
+  /*Card Overlay*/
+
+  $('.card').on('click', function () {
+    var id = $(this).attr('id');
+    console.log(id);
+    $('.overlayContainer').css('display', 'flex').hide().fadeIn(300);
+    $(`#${id}.overlay`).removeClass('overlayOff inactive').addClass('overlayOn active');
   });
 
+  $('.overlayContainer').on('click', function () {
+    var id = $('.active').attr('id');
+    var src = $('.gameContent').attr("src");
+    console.log(id, src);
+    $(`#${id}.overlay.overlayOn`).removeClass('overlayOn active').addClass('overlayOff inactive');
+    $('#gameContent.videoPlayer').attr("src", src);
+    $('.overlayContainer').fadeOut(1500);
+  });
 });
